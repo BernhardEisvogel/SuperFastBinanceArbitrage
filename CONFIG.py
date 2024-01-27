e77cfe97592cfa9b0e09e8c8720276f5a0c796ef
Bernhard Eisvogel <64311469+BernhardEisvogel@users.noreply.github.com>
2024-01-27 15:53:05 +0100

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 18:43:53 2023
+
+@author: be
+"""
+
+def CONFIG():
+    api_key    = "KEY"
+    api_secret = "SECRET"
+    return api_key, api_secret
